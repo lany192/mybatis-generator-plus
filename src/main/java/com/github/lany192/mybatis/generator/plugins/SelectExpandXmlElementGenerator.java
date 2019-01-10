3098e42f05bebdcd73bceb6ce781c359dfdd90a3
lyg <lany192@qq.com>
2019-01-10 18:22:11 +0800

    update

--- src/main/java/com/github/lany192/mybatis/generator/plugins/SelectExpandXmlElementGenerator.java
+++ src/main/java/com/github/lany192/mybatis/generator/plugins/SelectExpandXmlElementGenerator.java
@@ -12,106 +12,150 @@ import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElem
  */
 public class SelectExpandXmlElementGenerator extends AbstractXmlElementGenerator {
 
-	@Override
-	public void addElements(XmlElement parentElement) {
-		// 增加base_query
-		XmlElement sql = new XmlElement("sql");
-		sql.addAttribute(new Attribute("id", "base_query"));
-		//在这里添加where条件
-		//设置trim标签
+    @Override
+    public void addElements(XmlElement parentElement) {
+        // 增加base_query
+        XmlElement sql = new XmlElement("sql");
+        sql.addAttribute(new Attribute("id", "base_query"));
+        //在这里添加where条件
+        //设置trim标签
         XmlElement selectTrimElement = new XmlElement("trim");
         selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));
-		//添加where和and
+        //添加where和and
         selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR"));
-		StringBuilder sb = new StringBuilder();
-		for(IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
-			//$NON-NLS-1$
-			XmlElement selectNotNullElement = new XmlElement("if");
-			sb.setLength(0);
-			sb.append("null != ");
-			sb.append(introspectedColumn.getJavaProperty());
-			selectNotNullElement.addAttribute(new Attribute("test", sb.toString()));
-			sb.setLength(0);
-			// 添加and
-			sb.append(" and ");
-			// 添加别名t
-			sb.append("t.");
-			sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
-			// 添加等号
-			sb.append(" = ");
-			sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+        StringBuilder sb = new StringBuilder();
+        for(IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+            //$NON-NLS-1$
+            XmlElement selectNotNullElement = new XmlElement("if");
+            sb.setLength(0);
+            sb.append("null != ");
+            sb.append(introspectedColumn.getJavaProperty());
+            selectNotNullElement.addAttribute(new Attribute("test", sb.toString()));
+            sb.setLength(0);
+            // 添加and
+            sb.append(" and ");
+            // 添加别名t
+            sb.append("t.");
+            sb.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            // 添加等号
+            sb.append(" = ");
+            sb.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
             selectNotNullElement.addElement(new TextElement(sb.toString()));
             selectTrimElement.addElement(selectNotNullElement);
-		}
-		sql.addElement(selectTrimElement);
-		parentElement.addElement(sql);
-		
-		// 公用select
-		sb.setLength(0);
-		sb.append("select ");
-		sb.append("t.* ");
-		sb.append("from ");
-		sb.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
-		sb.append(" t");
-		TextElement selectText = new TextElement(sb.toString());
-		
-		// 公用include
-		XmlElement include = new XmlElement("include");
-		include.addAttribute(new Attribute("refid", "base_query"));
-		
-		// 增加find
-		XmlElement find = new XmlElement("select");
-		find.addAttribute(new Attribute("id", "selectOne"));
-		find.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		find.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		find.addElement(selectText);
-		find.addElement(include);
-		parentElement.addElement(find);
-		
-		// 增加list
-		XmlElement list = new XmlElement("select");
-		list.addAttribute(new Attribute("id", "selectByEntity"));
-		list.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		list.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		list.addElement(selectText);
-		list.addElement(include);
-		parentElement.addElement(list);
-		
-		// 增加pageList
-		XmlElement pageList = new XmlElement("select");
-		pageList.addAttribute(new Attribute("id", "selectByPage"));
-		pageList.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		pageList.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		pageList.addElement(selectText);
-		pageList.addElement(include);
-		parentElement.addElement(pageList);
+        }
+        sql.addElement(selectTrimElement);
+        parentElement.addElement(sql);
 
-		addSelectOneByExampleElement(parentElement);
-	}
+        // 公用select
+        sb.setLength(0);
+        sb.append("select ");
+        sb.append("t.* ");
+        sb.append("from ");
+        sb.append(introspectedTable.getFullyQualifiedTableNameAtRuntime());
+        sb.append(" t");
+        TextElement selectText = new TextElement(sb.toString());
 
-	private void addSelectOneByExampleElement(XmlElement parentElement){
-		XmlElement answer = new XmlElement("select");
-		answer.addAttribute(new Attribute("id", "selectOneByExample"));
-		answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
-		answer.addAttribute(new Attribute("parameterType", introspectedTable.getExampleType()));
-		context.getCommentGenerator().addComment(answer);
+        // 公用include
+        XmlElement include = new XmlElement("include");
+        include.addAttribute(new Attribute("refid", "base_query"));
 
-		answer.addElement(new TextElement("select"));
+//        // 增加find
+//        XmlElement find = new XmlElement("select");
+//        find.addAttribute(new Attribute("id", "selectOne"));
+//        find.addAttribute(new Attribute("resultMap", "BaseResultMap"));
+//        find.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+//        find.addElement(selectText);
+//        find.addElement(include);
+//        parentElement.addElement(find);
 
-		answer.addElement(getBaseColumnListElement());
+        // 增加list
+        XmlElement list = new XmlElement("select");
+        list.addAttribute(new Attribute("id", "selectByEntity"));
+        list.addAttribute(new Attribute("resultMap", "BaseResultMap"));
+        list.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+        list.addElement(selectText);
+        list.addElement(include);
+        parentElement.addElement(list);
 
-		StringBuilder sb = new StringBuilder();
-		sb.setLength(0);
-		sb.append("from ");
-		sb.append(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime());
-		answer.addElement(new TextElement(sb.toString()));
-		answer.addElement(getExampleIncludeElement());
+        // 增加pageList
+        XmlElement pageList = new XmlElement("select");
+        pageList.addAttribute(new Attribute("id", "selectByPage"));
+        pageList.addAttribute(new Attribute("resultMap", "BaseResultMap"));
+        pageList.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+        pageList.addElement(selectText);
+        pageList.addElement(include);
+        parentElement.addElement(pageList);
 
-		XmlElement ifElement = new XmlElement("if");
-		ifElement.addAttribute(new Attribute("test", "orderByClause != null"));
-		ifElement.addElement(new TextElement("order by ${orderByClause}"));
-		answer.addElement(ifElement);
+        addSelectOneElement(parentElement);
+        addSelectOneByExampleElement(parentElement);
+    }
 
-		parentElement.addElement(answer);
-	}
+    private void addSelectOneElement(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("select");
+        answer.addAttribute(new Attribute("id", "selectOne"));
+        answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
+        answer.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+        context.getCommentGenerator().addComment(answer);
+
+        answer.addElement(new TextElement("select"));
+        answer.addElement(getBaseColumnListElement());
+        answer.addElement(new TextElement(" from "));
+        answer.addElement(new TextElement(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+        answer.addElement(getRecordWhereXmlElement());
+
+        parentElement.addElement(answer);
+    }
+
+    private XmlElement getRecordWhereXmlElement() {
+        //在这里添加where条件
+        //设置trim标签
+        XmlElement selectTrimElement = new XmlElement("trim");
+        selectTrimElement.addAttribute(new Attribute("prefix", "WHERE"));
+        //添加where和and
+        selectTrimElement.addAttribute(new Attribute("prefixOverrides", "AND | OR"));
+        StringBuilder sb2 = new StringBuilder();
+        for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+            XmlElement selectNotNullElement = new XmlElement("if");
+            sb2.setLength(0);
+            sb2.append("null != ");
+            sb2.append(introspectedColumn.getJavaProperty());
+            selectNotNullElement.addAttribute(new Attribute("test", sb2.toString()));
+            sb2.setLength(0);
+            // 添加and
+            sb2.append(" and ");
+            sb2.append(MyBatis3FormattingUtilities.getEscapedColumnName(introspectedColumn));
+            // 添加等号
+            sb2.append(" = ");
+            sb2.append(MyBatis3FormattingUtilities.getParameterClause(introspectedColumn));
+            selectNotNullElement.addElement(new TextElement(sb2.toString()));
+            selectTrimElement.addElement(selectNotNullElement);
+        }
+        return selectTrimElement;
+    }
+
+    private void addSelectOneByExampleElement(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("select");
+        answer.addAttribute(new Attribute("id", "selectOneByExample"));
+        answer.addAttribute(new Attribute("resultMap", "BaseResultMap"));
+        answer.addAttribute(new Attribute("parameterType", introspectedTable.getExampleType()));
+        context.getCommentGenerator().addComment(answer);
+
+        answer.addElement(new TextElement("select"));
+
+        answer.addElement(getBaseColumnListElement());
+
+        StringBuilder sb = new StringBuilder();
+        sb.setLength(0);
+        sb.append("from ");
+        sb.append(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime());
+        answer.addElement(new TextElement(sb.toString()));
+        answer.addElement(getExampleIncludeElement());
+
+        XmlElement ifElement = new XmlElement("if");
+        ifElement.addAttribute(new Attribute("test", "orderByClause != null"));
+        ifElement.addElement(new TextElement("order by ${orderByClause}"));
+        answer.addElement(ifElement);
+
+        parentElement.addElement(answer);
+    }
 }
