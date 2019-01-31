6b5db3c4738f99e612ef2ec4dc8f76974fc24127
lyg <lany192@qq.com>
2019-01-31 11:29:43 +0800

    修改插件

---
+++ src/main/java/com/github/lany192/mybatis/generator/Swagger2DocPlugin.java
@@ -0,0 +1,38 @@
+package com.github.lany192.mybatis.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * 实体增加api文档注解
+ */
+public class Swagger2DocPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        //topLevelClass.addImportedType("io.swagger.annotations.ApiModel");
+        topLevelClass.addImportedType("io.swagger.annotations.ApiModelProperty");
+
+//        String classAnnotation = "@ApiModel(value=\"" + topLevelClass.getType() + "\")";
+//        if (!topLevelClass.getAnnotations().contains(classAnnotation)) {
+//            topLevelClass.addAnnotation(classAnnotation);
+//        }
+
+        field.addAnnotation("@ApiModelProperty(value=\""
+                + introspectedColumn.getJavaProperty()
+                + introspectedColumn.getRemarks() + "\")");
+
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+}
