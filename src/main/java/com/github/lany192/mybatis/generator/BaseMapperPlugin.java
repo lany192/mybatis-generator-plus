3ced41a29ce69821490cf4145dea95bdac7d3cf2
lany192 <lany192@qq.com>
2019-10-26 13:59:36 +0800

    update

--- src/main/java/com/github/lany192/mybatis/generator/BaseMapperPlugin.java
+++ src/main/java/com/github/lany192/mybatis/generator/BaseMapperPlugin.java
@@ -5,10 +5,8 @@ import com.github.lany192.mybatis.generator.utils.Log;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.Interface;
-import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,41 +35,47 @@ public class BaseMapperPlugin extends BasePlugin {
         //例如:"my.mabatis.example.base.BaseMapper"
         FullyQualifiedJavaType superJavaType = new FullyQualifiedJavaType(rootMapperClass);
 
-        TableInfo info = new TableInfo(getContext(), introspectedTable);
+//        TableInfo info = new TableInfo(getContext(), introspectedTable);
 
-        FullyQualifiedJavaType superInterface;
-        if (info.isHasBlob()) {
-            //主键默认采用java.lang.Long
-            superInterface = new FullyQualifiedJavaType(superJavaType.getShortName() + "<"
-                    + introspectedTable.getBaseRecordType() + ","
-                    + introspectedTable.getRecordWithBLOBsType() + ","
-                    + introspectedTable.getExampleType() + ","
-                    + "java.lang.Long" + ">");
-        } else {
-            //主键默认采用java.lang.Long
-            superInterface = new FullyQualifiedJavaType(superJavaType.getShortName() + "<"
-                    + introspectedTable.getBaseRecordType() + ","
-                    + introspectedTable.getBaseRecordType() + ","
-                    + introspectedTable.getExampleType() + ","
-                    + "java.lang.Long" + ">");
-        }
+        FullyQualifiedJavaType superInterface = new FullyQualifiedJavaType(superJavaType.getShortName() + "<"
+                + introspectedTable.getBaseRecordType() + ","
+                + introspectedTable.getExampleType() + ","
+                + "java.lang.Long" + ">");
+//        if (info.isHasBlob()) {
+//            //主键默认采用java.lang.Long
+//            superInterface = new FullyQualifiedJavaType(superJavaType.getShortName() + "<"
+//                    + introspectedTable.getBaseRecordType() + ","
+//                    + introspectedTable.getRecordWithBLOBsType() + ","
+//                    + introspectedTable.getExampleType() + ","
+//                    + "java.lang.Long" + ">");
+//        } else {
+//            //主键默认采用java.lang.Long
+//            superInterface = new FullyQualifiedJavaType(superJavaType.getShortName() + "<"
+//                    + introspectedTable.getBaseRecordType() + ","
+//                    + introspectedTable.getBaseRecordType() + ","
+//                    + introspectedTable.getExampleType() + ","
+//                    + "java.lang.Long" + ">");
+//        }
+
+
         //添加 extends BaseMapper
         interfaze.addSuperInterface(superInterface);
         //添加import my.mabatis.example.base.BaseMapper;
         interfaze.addImportedType(superJavaType);
-        //清除不要的方法
-        if (info.isHasBlob()) {
-            List<Method> methods = interfaze.getMethods();
-            List<Method> clearMethods = new ArrayList<>();
-            for (Method method : methods) {
-                if (!method.getName().contains("WithBLOBs")) {
-                    clearMethods.add(method);
-                }
-            }
-            interfaze.getMethods().removeAll(clearMethods);
-        } else {
-            interfaze.getMethods().clear();
-        }
+//        //清除不要的方法
+//        if (info.isHasBlob()) {
+//            List<Method> methods = interfaze.getMethods();
+//            List<Method> clearMethods = new ArrayList<>();
+//            for (Method method : methods) {
+//                if (!method.getName().contains("WithBLOBs")) {
+//                    clearMethods.add(method);
+//                }
+//            }
+//            interfaze.getMethods().removeAll(clearMethods);
+//        } else {
+//            interfaze.getMethods().clear();
+//        }
+        interfaze.getMethods().clear();
         //interfaze.getAnnotations().clear();
         return true;
     }
