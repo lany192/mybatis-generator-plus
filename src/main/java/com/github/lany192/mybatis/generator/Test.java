3eeb0f390f8a0bebb5bed227e53951db0a387418
lany192 <lany192@qq.com>
2020-03-24 22:45:50 +0800

    update

---
+++ src/main/java/com/github/lany192/mybatis/generator/Test.java
@@ -0,0 +1,14 @@
+package com.github.lany192.mybatis.generator;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(byte[].class.getName());
+        try {
+            Class clazz = Class.forName("[B");
+            System.out.println(byte[].class==clazz);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
