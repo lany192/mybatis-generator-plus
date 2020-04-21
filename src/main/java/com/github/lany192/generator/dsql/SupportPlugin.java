e3a9df283593d8ce68730f8f49fb545483d4b0a6
lany192 <lany192@qq.com>
2020-04-21 09:25:19 +0800

    update gs

--- src/main/java/com/github/lany192/generator/dsql/SupportPlugin.java
+++ src/main/java/com/github/lany192/generator/dsql/SupportPlugin.java
@@ -3,7 +3,6 @@ package com.github.lany192.generator.dsql;
 import com.github.lany192.generator.BasePlugin;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.internal.util.StringUtility;
-import org.mybatis.generator.internal.util.messages.Messages;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -13,12 +12,9 @@ import java.util.regex.Pattern;
  * 修改DynamicSqlSupport类的目标地址
  */
 public class SupportPlugin extends BasePlugin {
-    /**
-     * 目标包
-     */
     private String targetPackage;
     private String replaceString;
-    private Pattern pattern;
+    private String searchString;
 
     @Override
     public boolean validate(List<String> warnings) {
@@ -27,37 +23,36 @@ public class SupportPlugin extends BasePlugin {
             warnings.add(this.getClass().getTypeName() + "插件要求运行targetRuntime必须为MyBatis3DynamicSQL！");
             return false;
         }
-        if (!check("target_package")) {
-            warnings.add("请配置" + this.getClass().getName() + "插件的目标包名属性：" + "target_package");
-            return false;
-        }
-        this.targetPackage = getProperty("target_package", "");
-        String searchString = getProperty("searchString", "");
+        this.targetPackage = getProperty("targetPackage", "");
+        this.searchString = getProperty("searchString", "");
         this.replaceString = getProperty("replaceString", "");
-        boolean valid = StringUtility.stringHasValue(searchString) && StringUtility.stringHasValue(this.replaceString);
-        if (valid) {
-            this.pattern = Pattern.compile(searchString);
-        } else {
-            if (!StringUtility.stringHasValue(searchString)) {
-                warnings.add(Messages.getString("ValidationError.18", "RenameDynamicSqlSupportClassPlugin", "searchString"));
-            }
-
+        if (StringUtility.stringHasValue(searchString)) {
             if (!StringUtility.stringHasValue(this.replaceString)) {
-                warnings.add(Messages.getString("ValidationError.18", "RenameDynamicSqlSupportClassPlugin", "replaceString"));
+                warnings.add(this.getClass().getTypeName() + "插件:请配置replaceString参数！");
+                return false;
+            }
+        } else {
+            if (StringUtility.stringHasValue(this.replaceString)) {
+                warnings.add(this.getClass().getTypeName() + "插件:请配置searchString参数！");
+                return false;
             }
         }
-        return valid;
+        return true;
     }
 
     @Override
     public void initialized(IntrospectedTable introspectedTable) {
         super.initialized(introspectedTable);
-        String oldType = introspectedTable.getMyBatisDynamicSqlSupportType();
+        String supportType = introspectedTable.getMyBatisDynamicSqlSupportType();
         //修改包名
-        String newType = oldType.replace(getContext().getJavaClientGeneratorConfiguration().getTargetPackage(), this.targetPackage);
+        if (!StringUtility.stringHasValue(this.targetPackage)) {
+            supportType = supportType.replace(getContext().getJavaClientGeneratorConfiguration().getTargetPackage(), this.targetPackage);
+        }
         //修改类名
-        Matcher matcher = this.pattern.matcher(newType);
-        newType = matcher.replaceAll(this.replaceString);
-        introspectedTable.setMyBatisDynamicSqlSupportType(newType);
+        if (!StringUtility.stringHasValue(this.searchString)) {
+            Matcher matcher = Pattern.compile(searchString).matcher(supportType);
+            supportType = matcher.replaceAll(this.replaceString);
+        }
+        introspectedTable.setMyBatisDynamicSqlSupportType(supportType);
     }
 }
