1b7f3fe4a3e0d07cc2cb3f7a84bd7ec7af171ae0
DESKTOP-QO0TDI7\Lany <lany92@qq.com>
2020-07-05 16:06:22 +0800

    update

---
+++ src/main/java/com/github/lany192/generator/other/JsonPlugin.java
@@ -0,0 +1,35 @@
+package com.github.lany192.generator.other;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+public class JsonPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        if (introspectedColumn.getJdbcTypeName().equals("java.time.LocalDateTime")) {
+            topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
+            field.addAnnotation("@JsonFormat(pattern = \"yyyy-MM-dd HH:mm:ss\")");
+        }
+        if (introspectedColumn.getJdbcTypeName().equals("java.time.LocalTime")) {
+            topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
+            field.addAnnotation("@JsonFormat(pattern = \"HH:mm:ss\")");
+        }
+        if (introspectedColumn.getJdbcTypeName().equals("java.time.LocalDate")) {
+            topLevelClass.addImportedType("com.fasterxml.jackson.annotation.JsonFormat");
+            field.addAnnotation("@JsonFormat(pattern = \"yyyy-MM-dd\")");
+        }
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+}
