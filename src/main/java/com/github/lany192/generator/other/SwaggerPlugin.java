9cb5d4d166f9b06a74587a0412aa28ff27a10452
lany192 <lany192@qq.com>
2020-12-06 21:32:16 +0800

    update

--- src/main/java/com/github/lany192/generator/other/SwaggerPlugin.java
+++ src/main/java/com/github/lany192/generator/other/SwaggerPlugin.java
@@ -21,13 +21,13 @@ public class SwaggerPlugin extends PluginAdapter {
     @Override
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
                                        IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        //topLevelClass.addImportedType("io.swagger.annotations.ApiModel");
+        topLevelClass.addImportedType("io.swagger.annotations.ApiModel");
         topLevelClass.addImportedType("io.swagger.annotations.ApiModelProperty");
 
-//        String classAnnotation = "@ApiModel(value=\"" + topLevelClass.getType() + "\")";
-//        if (!topLevelClass.getAnnotations().contains(classAnnotation)) {
-//            topLevelClass.addAnnotation(classAnnotation);
-//        }
+        String classAnnotation = "@ApiModel(value=\"" + topLevelClass.getType().getShortName() + "\", description = \"" + introspectedTable.getRemarks() + "\")";
+        if (!topLevelClass.getAnnotations().contains(classAnnotation)) {
+            topLevelClass.addAnnotation(classAnnotation);
+        }
 
         field.addAnnotation("@ApiModelProperty(value=\"" + introspectedColumn.getRemarks() + "\")");
         return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
