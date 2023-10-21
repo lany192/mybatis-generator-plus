7961d7c4634d6e645eda0704a03042f1e0a35b3f
lany <lany192@qq.com>
2023-10-21 15:57:27 +0800

    update

---
+++ src/main/java/com/github/lany192/generator/other/ValidatedPlugin.java
@@ -0,0 +1,32 @@
+package com.github.lany192.generator.other;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * 检查校验字段
+ */
+public class ValidatedPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        String comment = introspectedTable.getFullyQualifiedTable().toString() + "." + introspectedColumn.getActualColumnName();
+        field.addJavaDocLine("/**\n" +
+                "     * " + introspectedColumn.getRemarks() + "\n" +
+                "     * 表字段：" + comment + "\n" +
+                "     * 类型：{@link " + introspectedColumn.getFullyQualifiedJavaType().getFullyQualifiedName() + "}\n" +
+                "     */");
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+}
