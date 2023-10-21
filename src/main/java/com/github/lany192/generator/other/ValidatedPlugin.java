3a7ad4329e514477389b99ca01b5097d196472ae
lany192 <lany192@qq.com>
2023-10-21 21:53:20 +0800

    update

--- src/main/java/com/github/lany192/generator/other/ValidatedPlugin.java
+++ src/main/java/com/github/lany192/generator/other/ValidatedPlugin.java
@@ -4,9 +4,11 @@ import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 检查校验字段
@@ -22,7 +24,13 @@ public class ValidatedPlugin extends PluginAdapter {
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
                                        IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         if (!introspectedColumn.isNullable()) {
-            field.addAnnotation("@NoBlank()");
+            if (Objects.equals(introspectedColumn.getFullyQualifiedJavaType(), new FullyQualifiedJavaType(String.class.getTypeName()))) {
+                topLevelClass.addImportedType("jakarta.validation.constraints.NotBlank");
+                field.addAnnotation("@NotBlank(message = \"" + introspectedTable.getRemarks() + "不能为空\")");
+            } else {
+                topLevelClass.addImportedType("jakarta.validation.constraints.NotNull");
+                field.addAnnotation("@NotNull(message = \"" + introspectedTable.getRemarks() + "不能为null\")");
+            }
         }
         return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
     }
